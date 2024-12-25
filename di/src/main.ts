9c3028ce912c01a290c3a2a583a296353e79a1b7
KonstantinNatev <76740873+KonstantinNatev@users.noreply.github.com>
2024-12-25 21:39:15 +0200

    create new project - di

---
+++ di/src/main.ts
@@ -0,0 +1,8 @@
+import { NestFactory } from '@nestjs/core';
+import { ComputerModule } from './computer/computer.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(ComputerModule);
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
